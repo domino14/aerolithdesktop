f388917d9b8ab7451efc43857a2597353f75aad1
cesar <cesar@93b1bf58-7229-3f38-526f-075f678e574b>
2007-07-27 19:09:57 +0000

    cool

--- aerolith/server/src/main.cpp
+++ aerolith/server/src/main.cpp
@@ -4,8 +4,44 @@
 
 static const quint16 DEFAULT_PORT = 1988;
 
+QTextStream *outFile = 0;
+
+void myMessageOutput(QtMsgType type, const char *msg)
+{
+   QString debugdate = 
+     QDateTime::currentDateTime().toString(QLatin1String("[dd.MM.yy hh:mm:ss] "));
+  switch (type)
+    {
+    case QtDebugMsg:
+      debugdate += "[D]";
+      break;
+    case QtWarningMsg:
+      debugdate += "[W]";
+      break;
+    case QtCriticalMsg:
+      debugdate += "[C]";
+      break;
+    case QtFatalMsg:
+      debugdate += "[F]";
+    }
+
+  (*outFile) << debugdate << " " << msg << endl;
+}
+
 int main (int argc, char **argv)
 {
+  QFile *log = new QFile("log.txt");
+  if (log->open(QIODevice::WriteOnly))
+  {
+    outFile = new QTextStream (log);
+    qInstallMsgHandler(myMessageOutput);
+  }
+  else
+  {
+    delete log;
+    qDebug("Can't open log.txt file, all message will be output to debugger and console.");
+  }
+
   QCoreApplication app(argc, argv);
   quint16 port = 0;
   bool ok = false;
