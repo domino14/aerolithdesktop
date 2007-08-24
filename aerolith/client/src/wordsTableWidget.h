f2b16921b60d477f40fdf116411967e5cf995632
cesar <cesar@93b1bf58-7229-3f38-526f-075f678e574b>
2007-08-24 16:42:17 +0000

    aaaa

--- aerolith/client/src/wordsTableWidget.h
+++ aerolith/client/src/wordsTableWidget.h
@@ -19,10 +19,12 @@ Q_OBJECT
   QBrush colorBrushes[9]; // add to utilities file
   void answeredCorrectly(int, int);
   void setCellProperties(int i, int j, QString alphagram, QStringList solutions, quint8 numSolutions);
-
-
+  quint8 getColorBrushIndex(quint8 numSolutions);
+  void changeFontSize(QFont&);
+  quint8 wordLength;
  private:
-
+  
+  bool fixedWidth;
   QString shuffleString(QString);
 
   QTableWidgetItem* wordCells[FIXED_ROWS][FIXED_COLS];
