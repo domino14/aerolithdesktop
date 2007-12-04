7d54bda13dcd758da5bdc1cbcc005c8d2cd928d3
cesar <cesar@93b1bf58-7229-3f38-526f-075f678e574b>
2007-12-04 22:19:53 +0000

    Created tag from server, 0.3.1. Also this will be the new working copy.

---
+++ aerolith/tags/server/0.3.1/src/table.h
@@ -0,0 +1,44 @@
+#ifndef _TABLE_H_
+#define _TABLE_H_
+
+#include <QtCore>
+//#include "TableGame.h"
+//#include "UnscrambleGame.h"
+#include "ClientSocket.h"
+
+
+class TableGame;
+
+
+class tableData
+{
+ public:
+  enum gameModes
+    { GAMEMODE_UNSCRAMBLE, GAMEMODE_ANAGRAMS, GAMEMODE_TRIVIA };
+
+ enum tablePacketHeaders
+   {
+     GAME_STARTED, GAME_ENDED, CHAT_SENT, GUESS_RIGHT, TIMER_VALUE, READY_TO_BEGIN, GAVE_UP
+   };
+ 
+  ~tableData();
+  void initialize(quint16 tableNumber, QString tableName, quint8 maxPlayers, 
+		  ClientSocket* tableCreator, quint8 cycleState, quint8 tableTimer, gameModes gameMode, QString additionalDescriptor);
+  quint16 tableNumber;
+  //QString wordListDescriptor; // todo: change this to table name
+  QString tableName;
+  QList <ClientSocket*> playerList;
+  quint8 maxPlayers;
+  bool canJoin; 
+  TableGame* tableGame;
+  
+  void sendChatSentPacket(QString, QString);
+  void sendGenericPacket();
+  void sendTableMessage(QString);
+  // anagrams mode?
+  gameModes gameMode; 
+  QByteArray letters; // the letters that are showing on the board
+
+};
+
+#endif
