e5e4945035f01c3bc5f58cf94a634f3f6d8e0be7
cesar <cesar@93b1bf58-7229-3f38-526f-075f678e574b>
2007-12-06 07:44:43 +0000

    adding new poker-style table interface.
    not yet working.

--- trunk/client/src/UnscrambleGameTable.cpp
+++ trunk/client/src/UnscrambleGameTable.cpp
@@ -3,122 +3,147 @@
 UnscrambleGameTable::UnscrambleGameTable(QWidget* parent, Qt::WindowFlags f) : QWidget(parent, f)
 {
 
-  wordsWidget = new wordsTableWidget();
-  // solution box
-  QLabel *solutionLabel = new QLabel("Guess:");
-  solutionLE = new QLineEdit;
-  solutionLE->setFixedWidth(100);
-  solutionLE->setMaxLength(15);
-  solutions = new QPushButton("Solutions");
-  QPushButton *alpha = new QPushButton("Alpha");
-  QPushButton *shuffle = new QPushButton("Shuffle");
-  giveup = new QPushButton("Give up");
-  start = new QPushButton("Start");
-  exitTable = new QPushButton("Exit Table #");
-  QPushButton *changeFont = new QPushButton("Font toggle");
-  QPushButton *plusFont = new QPushButton("+");
-  QPushButton *minusFont = new QPushButton("-");
+	wordsWidget = new wordsTableWidget(5, 5);
+	// solution box
+	QLabel *solutionLabel = new QLabel("Guess:");
+	solutionLE = new QLineEdit;
+	solutionLE->setFixedWidth(100);
+	solutionLE->setMaxLength(15);
+	solutions = new QPushButton("Solutions");
+	QPushButton *alpha = new QPushButton("Alpha");
+	QPushButton *shuffle = new QPushButton("Shuffle");
+	giveup = new QPushButton("Give up");
+	start = new QPushButton("Start");
+	exitTable = new QPushButton("Exit Table #");
+	QPushButton *changeFont = new QPushButton("Font toggle");
+	QPushButton *plusFont = new QPushButton("+");
+	QPushButton *minusFont = new QPushButton("-");
 
+
+	connect(changeFont, SIGNAL(clicked()), wordsWidget, SLOT(changeFont()));
+	//  connect(plusFont, SIGNAL(clicked()), wordsWidget, SLOT(increaseFontSize()));
+	//connect(minusFont, SIGNAL(clicked()), wordsWidget, SLOT(decreaseFontSize()));
+
+
+	solutions->setFocusPolicy(Qt::NoFocus);
+	alpha->setFocusPolicy(Qt::NoFocus);
+	shuffle->setFocusPolicy(Qt::NoFocus);
+	giveup->setFocusPolicy(Qt::NoFocus);
+	start->setFocusPolicy(Qt::NoFocus);
+	exitTable->setFocusPolicy(Qt::NoFocus);
+	changeFont->setFocusPolicy(Qt::NoFocus);
+	QHBoxLayout *topSolutionLayout = new QHBoxLayout;
+
+	timerDial = new QLCDNumber(4);
+	timerDial->setFixedWidth(50);
+	timerDial->setSegmentStyle(QLCDNumber::Flat);
+
+	wordListInfo = new QLabel;
+	wordListInfo->setFixedWidth(100);
+	topSolutionLayout->addWidget(timerDial);
+	topSolutionLayout->addSpacing(50);
+	topSolutionLayout->addWidget(wordListInfo);
+	topSolutionLayout->addSpacing(50);
+	topSolutionLayout->addWidget(changeFont);
+	topSolutionLayout->addStretch(1);
+
+	topSolutionLayout->addWidget(giveup);
+	topSolutionLayout->addSpacing(50);
+	topSolutionLayout->addWidget(exitTable);
+	QHBoxLayout *bottomSolutionLayout = new QHBoxLayout;
+
+	bottomSolutionLayout->addWidget(solutionLabel);
+	bottomSolutionLayout->addWidget(solutionLE);
+	bottomSolutionLayout->addStretch(1);
+	//bottomSolutionLayout->addWidget(timerDial);
+	bottomSolutionLayout->addWidget(start);
+	bottomSolutionLayout->addWidget(solutions);
+	bottomSolutionLayout->addWidget(alpha);
+	bottomSolutionLayout->addWidget(shuffle);
+	//solutionLayout->addSpacing(50);
+	//solutionLayout->addWidget(giveup);
+	//solutionLayout->addWidget(exitTable);
+
+	// Players box
+
+	playerInfoWidget = new PlayerInfoWidget(); // includes lists, etc
+
+	QVBoxLayout *gameBoardLayout = new QVBoxLayout;
+	gameBoardLayout->addLayout(topSolutionLayout);
+	gameBoardLayout->addWidget(wordsWidget, 0, Qt::AlignHCenter);
+	gameBoardLayout->addLayout(bottomSolutionLayout);
+	gameBoardLayout->addWidget(playerInfoWidget);
+
+	peopleInTable = new QListWidget;
+	chatLE = new QLineEdit;
+	tableChat = new QTextEdit;
+
+	connect(peopleInTable, SIGNAL(itemDoubleClicked(QListWidgetItem*)), this, SLOT(sendPM(QListWidgetItem* )));
+	chatLE->setMaxLength(300);
+	tableChat->setReadOnly(true);
+	tableChat->setTextInteractionFlags(Qt::TextSelectableByMouse);
+
+	QHBoxLayout *chatBoxLayout = new QHBoxLayout;
+	chatBoxLayout->addWidget(tableChat);
+	tableChat->setFrameShape(QFrame::Box);
+	peopleInTable->setFrameShape(QFrame::Box);
+	peopleInTable->setFixedWidth(150);
+	chatBoxLayout->addWidget(peopleInTable);
+
+	gameBoardLayout->addWidget(chatLE);
+	gameBoardLayout->addSpacing(5);
+	gameBoardLayout->addLayout(chatBoxLayout);
+	connect(alpha, SIGNAL(clicked()), wordsWidget, SLOT(alphagrammizeWords()));
+	connect(shuffle, SIGNAL(clicked()), wordsWidget, SLOT(shuffleWords()));
+
+	//  setLayout(gameBoardLayout);
+
+	setTabOrder(solutionLE, chatLE);
+	setTabOrder(chatLE, solutionLE);
+	tableChat->setFocusPolicy(Qt::ClickFocus);
+	peopleInTable->setFocusPolicy(Qt::NoFocus);  
+
+
+	connect(giveup, SIGNAL(clicked()), this, SIGNAL(giveUp()));
+	connect(start, SIGNAL(clicked()), this, SIGNAL(sendStartRequest()));
+	connect(playerInfoWidget, SIGNAL(avatarChange(quint8)), this, SIGNAL(avatarChange(quint8)));
+	connect(solutionLE, SIGNAL(returnPressed()), this, SLOT(enteredGuess()));
+	connect(exitTable, SIGNAL(clicked()), this, SIGNAL(exitThisTable()));
+
+	connect(chatLE, SIGNAL(returnPressed()), this, SLOT(enteredChat()));
+
+
+	tableUi.setupUi(this);
+
+	gfxScene.setSceneRect(0, 0, 900, 600);
+	tableUi.graphicsView->setScene(&gfxScene);	  	
+
+	tableUi.graphicsView->setBackgroundBrush(QImage(":/images/canvas.png"));
+
+	tableUi.graphicsView->setCacheMode(QGraphicsView::CacheBackground); 
+	QGraphicsPixmapItem* tableItem = gfxScene.addPixmap(QPixmap(":/images/table.png"));
+	//tableItem->setFlags(QGraphicsItem::ItemIsMovable);
+	tableItem->setOffset(QPoint(70, 20));
+
+	QGraphicsPixmapItem* aItem = gfxScene.addPixmap(QPixmap(":/images/A.png"));
+	aItem->setFlags(QGraphicsItem::ItemIsMovable);
+
+	QGraphicsPixmapItem* bItem = gfxScene.addPixmap(QPixmap(":/images/chip1.png"));
+	bItem->setFlags(QGraphicsItem::ItemIsMovable);
 	
-  connect(changeFont, SIGNAL(clicked()), wordsWidget, SLOT(changeFont()));
-  //  connect(plusFont, SIGNAL(clicked()), wordsWidget, SLOT(increaseFontSize()));
-  //connect(minusFont, SIGNAL(clicked()), wordsWidget, SLOT(decreaseFontSize()));
+	QGraphicsPixmapItem* cItem = gfxScene.addPixmap(QPixmap(":/images/chip9plus.png"));
+	cItem->setFlags(QGraphicsItem::ItemIsMovable);
 
-  
-  solutions->setFocusPolicy(Qt::NoFocus);
-  alpha->setFocusPolicy(Qt::NoFocus);
-  shuffle->setFocusPolicy(Qt::NoFocus);
-  giveup->setFocusPolicy(Qt::NoFocus);
-  start->setFocusPolicy(Qt::NoFocus);
-  exitTable->setFocusPolicy(Qt::NoFocus);
-  changeFont->setFocusPolicy(Qt::NoFocus);
-  QHBoxLayout *topSolutionLayout = new QHBoxLayout;
-  
-  timerDial = new QLCDNumber(4);
-  timerDial->setFixedWidth(50);
-  timerDial->setSegmentStyle(QLCDNumber::Flat);
-  
-  wordListInfo = new QLabel;
-  wordListInfo->setFixedWidth(100);
-  topSolutionLayout->addWidget(timerDial);
-  topSolutionLayout->addSpacing(50);
-  topSolutionLayout->addWidget(wordListInfo);
-  topSolutionLayout->addSpacing(50);
-  topSolutionLayout->addWidget(changeFont);
-  topSolutionLayout->addStretch(1);
-  
-  topSolutionLayout->addWidget(giveup);
-  topSolutionLayout->addSpacing(50);
-  topSolutionLayout->addWidget(exitTable);
-  QHBoxLayout *bottomSolutionLayout = new QHBoxLayout;
-  
-  bottomSolutionLayout->addWidget(solutionLabel);
-  bottomSolutionLayout->addWidget(solutionLE);
-  bottomSolutionLayout->addStretch(1);
-  //bottomSolutionLayout->addWidget(timerDial);
-  bottomSolutionLayout->addWidget(start);
-  bottomSolutionLayout->addWidget(solutions);
-  bottomSolutionLayout->addWidget(alpha);
-  bottomSolutionLayout->addWidget(shuffle);
-  //solutionLayout->addSpacing(50);
-  //solutionLayout->addWidget(giveup);
-  //solutionLayout->addWidget(exitTable);
-  
-  // Players box
-  
-  playerInfoWidget = new PlayerInfoWidget(); // includes lists, etc
-  
-  QVBoxLayout *gameBoardLayout = new QVBoxLayout;
-  gameBoardLayout->addLayout(topSolutionLayout);
-  gameBoardLayout->addWidget(wordsWidget, 0, Qt::AlignHCenter);
-  gameBoardLayout->addLayout(bottomSolutionLayout);
-  gameBoardLayout->addWidget(playerInfoWidget);
+	setWindowIcon(QIcon(":/images/aerolith.png"));
+	this->setWindowFlags(Qt::Dialog);
 
-  peopleInTable = new QListWidget;
-  chatLE = new QLineEdit;
-  tableChat = new QTextEdit;
-
-   connect(peopleInTable, SIGNAL(itemDoubleClicked(QListWidgetItem*)), this, SLOT(sendPM(QListWidgetItem* )));
-  chatLE->setMaxLength(300);
-  tableChat->setReadOnly(true);
-  tableChat->setTextInteractionFlags(Qt::TextSelectableByMouse);
-  	
-  QHBoxLayout *chatBoxLayout = new QHBoxLayout;
-  chatBoxLayout->addWidget(tableChat);
-  tableChat->setFrameShape(QFrame::Box);
-  peopleInTable->setFrameShape(QFrame::Box);
-  peopleInTable->setFixedWidth(150);
-  chatBoxLayout->addWidget(peopleInTable);
-  
-  gameBoardLayout->addWidget(chatLE);
-  gameBoardLayout->addSpacing(5);
-  gameBoardLayout->addLayout(chatBoxLayout);
-  connect(alpha, SIGNAL(clicked()), wordsWidget, SLOT(alphagrammizeWords()));
-  connect(shuffle, SIGNAL(clicked()), wordsWidget, SLOT(shuffleWords()));
-
-  setLayout(gameBoardLayout);
-
-  setTabOrder(solutionLE, chatLE);
-  setTabOrder(chatLE, solutionLE);
-  tableChat->setFocusPolicy(Qt::ClickFocus);
-  peopleInTable->setFocusPolicy(Qt::NoFocus);  
-
-
-  connect(giveup, SIGNAL(clicked()), this, SIGNAL(giveUp()));
-  connect(start, SIGNAL(clicked()), this, SIGNAL(sendStartRequest()));
-  connect(playerInfoWidget, SIGNAL(avatarChange(quint8)), this, SIGNAL(avatarChange(quint8)));
-  connect(solutionLE, SIGNAL(returnPressed()), this, SLOT(enteredGuess()));
-  connect(exitTable, SIGNAL(clicked()), this, SIGNAL(exitThisTable()));
-
-  connect(chatLE, SIGNAL(returnPressed()), this, SLOT(enteredChat()));
-  //  connect(
-  
 }
 
 void UnscrambleGameTable::enteredChat()
 {
-  emit chatTable(chatLE->text());
-  chatLE->clear();
+	emit chatTable(chatLE->text());
+	chatLE->clear();
+
 }
 
 void UnscrambleGameTable::sendPM(QListWidgetItem* item)
@@ -131,54 +156,55 @@ void UnscrambleGameTable::sendPM(QListWidgetItem* item)
 void UnscrambleGameTable::enteredGuess()
 {
 
-  emit guessSubmitted(solutionLE->text());
-  solutionLE->clear();
+	emit guessSubmitted(solutionLE->text());
+	solutionLE->clear();
 }
 
 void UnscrambleGameTable::closeEvent(QCloseEvent* event)
 {
-  event->ignore();
-  exitTable->animateClick();
+	event->ignore();
+	exitTable->animateClick();
+
 }
 
 void UnscrambleGameTable::resetTable(quint16 tableNum, QString wordListName, QString myUsername)
 {
-  setWindowTitle(QString("Table %1 - Word List: %2 - Logged in as %3").arg(tableNum).arg(wordListName).arg(myUsername));
-  timerDial->display(0);
-  wordListInfo->clear();
-  playerInfoWidget->clearAndHide();
-  wordsWidget->clearCells();
-  exitTable->setText(QString("Exit table %1").arg(tableNum));
-  chatLE->clear();
-  tableChat->clear();
+	setWindowTitle(QString("Table %1 - Word List: %2 - Logged in as %3").arg(tableNum).arg(wordListName).arg(myUsername));
+	timerDial->display(0);
+	wordListInfo->clear();
+	playerInfoWidget->clearAndHide();
+	wordsWidget->clearCells();
+	exitTable->setText(QString("Exit table %1").arg(tableNum));
+	chatLE->clear();
+	tableChat->clear();
 }
 
 void UnscrambleGameTable::leaveTable()
 {
-  playerInfoWidget->leaveTable();
-  peopleInTable->clear();
-  
+	playerInfoWidget->leaveTable();
+	peopleInTable->clear();
+
 }
 
 void UnscrambleGameTable::addPlayers(QStringList plist)
 {
-  playerInfoWidget->addPlayers(plist);
-  peopleInTable->addItems(plist);
+	playerInfoWidget->addPlayers(plist);
+	peopleInTable->addItems(plist);
 }
 
 void UnscrambleGameTable::addPlayer(QString player, bool gameStarted)
 {
-  playerInfoWidget->addPlayer(player, gameStarted);
-  peopleInTable->addItem(player);
+	playerInfoWidget->addPlayer(player, gameStarted);
+	peopleInTable->addItem(player);
 }
 
 void UnscrambleGameTable::removePlayer(QString player, bool gameStarted)
 {
-  playerInfoWidget->removePlayer(player, gameStarted);
-  for (int i = 0; i < peopleInTable->count(); i++)
-    if (peopleInTable->item(i)->text() == player)
-      {
-	QListWidgetItem *it = peopleInTable->takeItem(i);
-	delete it;
-      }
+	playerInfoWidget->removePlayer(player, gameStarted);
+	for (int i = 0; i < peopleInTable->count(); i++)
+		if (peopleInTable->item(i)->text() == player)
+		{
+			QListWidgetItem *it = peopleInTable->takeItem(i);
+			delete it;
+		}
 }
--- trunk/client/src/wordsTableWidget.cpp
+++ trunk/client/src/wordsTableWidget.cpp
@@ -3,188 +3,199 @@
 
 
 
-wordsTableWidget::wordsTableWidget()
+wordsTableWidget::wordsTableWidget(int numRows, int numCols)
 {
-  setRowCount(FIXED_ROWS);
-  setColumnCount(FIXED_COLS);
-  
-  horizontalHeader()->hide();
-  verticalHeader()->hide();
+	this->numRows = numRows;
+	this->numCols = numCols;
+	setRowCount(numRows);
+	setColumnCount(numCols);
 
-  setSelectionMode(QAbstractItemView::NoSelection);
-  setEditTriggers(QAbstractItemView::NoEditTriggers);
+	horizontalHeader()->hide();
+	verticalHeader()->hide();
 
-  for (int i = 0; i < FIXED_COLS; i++)
-    setColumnWidth(i, 150);
+	setSelectionMode(QAbstractItemView::NoSelection);
+	setEditTriggers(QAbstractItemView::NoEditTriggers);
 
-  for (int i = 0; i < FIXED_ROWS; i++)
-    setRowHeight(i, 20);
+	setGridStyle(Qt::NoPen);
 
-  /*
-#ifdef Q_OS_MAC
-  QFont wordFont("Helvetica", 16, QFont::Black);
-#else
-  QFont wordFont("Helvetica", 12, QFont::Black);
-#endif
-  */
-  QFont wordFont("Helvetica", 6, QFont::Black);
+	for (int i = 0; i < numCols; i++)
+		setColumnWidth(i, 150);
+
+	for (int i = 0; i < numRows; i++)
+		setRowHeight(i, 20);
+
+	/*
+	#ifdef Q_OS_MAC
+	QFont wordFont("Helvetica", 16, QFont::Black);
+	#else
+	QFont wordFont("Helvetica", 12, QFont::Black);
+	#endif
+	*/
+	QFont wordFont("Helvetica", 6, QFont::Black);
 
 
-  colorBrushes[0].setColor(Qt::black);
-  colorBrushes[1].setColor(Qt::darkGreen);
-  colorBrushes[2].setColor(Qt::blue);
-  colorBrushes[3].setColor(Qt::darkCyan);
-  colorBrushes[4].setColor(Qt::green);
-  colorBrushes[5].setColor(Qt::darkMagenta);
-  colorBrushes[6].setColor(Qt::darkRed);
-  colorBrushes[7].setColor(Qt::red);
-  colorBrushes[8].setColor(Qt::magenta);
-  
-  for (int i = 0; i < FIXED_ROWS; i++)
-    for (int j = 0; j < FIXED_COLS; j++)
-      {
-	wordCells[i][j] = new QTableWidgetItem("");
-	wordCells[i][j]->setTextAlignment(Qt::AlignCenter);
-	wordCells[i][j]->setFont(wordFont);
-	setItem(i, j, wordCells[i][j]);
-      }
-  setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-  setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-  setFixedSize(752, 182); // argh
-  setFocusPolicy(Qt::NoFocus);
+	colorBrushes[0].setColor(Qt::black);
+	colorBrushes[1].setColor(Qt::darkGreen);
+	colorBrushes[2].setColor(Qt::blue);
+	colorBrushes[3].setColor(Qt::darkCyan);
+	colorBrushes[4].setColor(Qt::green);
+	colorBrushes[5].setColor(Qt::darkMagenta);
+	colorBrushes[6].setColor(Qt::darkRed);
+	colorBrushes[7].setColor(Qt::red);
+	colorBrushes[8].setColor(Qt::magenta);
 
-  connect(this, SIGNAL(itemClicked(QTableWidgetItem*)), this, SLOT(wordsWidgetItemClicked(QTableWidgetItem*)));
-  fixedWidth = false;
+	for (int i = 0; i < numRows; i++)
+		for (int j = 0; j < numCols; j++)
+		{
+			wordsTableWidgetItem* item = new wordsTableWidgetItem;
+			item->tableWidgetItem = new QTableWidgetItem("");
+			item->tableWidgetItem->setTextAlignment(Qt::AlignCenter);
+			item->tableWidgetItem->setFont(wordFont);
+			setItem(i, j, item->tableWidgetItem);
+
+			tableItems.append(item);
+		}
+		setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+		setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+		setFixedSize(752, 182); // argh
+		setFocusPolicy(Qt::NoFocus);
+
+		connect(this, SIGNAL(itemClicked(QTableWidgetItem*)), this, SLOT(wordsWidgetItemClicked(QTableWidgetItem*)));
+		fixedWidth = false;
 }
 
 void wordsTableWidget::wordsWidgetItemClicked(QTableWidgetItem* clickedItem)
 {
-  int i = clickedItem->row();
-  int j = clickedItem->column();
-  item(i, j)->setText(shuffleString(item(i, j)->text()));
+	int i = clickedItem->row();
+	int j = clickedItem->column();
+	item(i, j)->setText(shuffleString(item(i, j)->text()));
 }
 
 void wordsTableWidget::changeFont()
 {
-  QFont wordFont;
+	QFont wordFont;
 
-  wordFont.setPointSize(6); // start here
-  if (fixedWidth == false)
-    {
-      wordFont.setFamily("Courier New");
-      wordFont.setStyleHint(QFont::TypeWriter);
-      wordFont.setWeight(QFont::Black);
-      
-      fixedWidth = true;
-    }
-  else
-    {
-      wordFont.setFamily("Helvetica");
-      wordFont.setStyleHint(QFont::SansSerif);
-      wordFont.setWeight(QFont::Black);
- 
-      fixedWidth = false;
-    }
+	wordFont.setPointSize(6); // start here
+	if (fixedWidth == false)
+	{
+		wordFont.setFamily("Courier New");
+		wordFont.setStyleHint(QFont::TypeWriter);
+		wordFont.setWeight(QFont::Black);
 
-  changeFontSize(wordFont);
+		fixedWidth = true;
+	}
+	else
+	{
+		wordFont.setFamily("Helvetica");
+		wordFont.setStyleHint(QFont::SansSerif);
+		wordFont.setWeight(QFont::Black);
 
-  for (int i = 0; i < FIXED_ROWS; i++)
-    for (int j = 0; j < FIXED_COLS; j++)
-      wordCells[i][j]->setFont(wordFont);
-  
+		fixedWidth = false;
+	}
+
+	changeFontSize(wordFont);
+
+	for (int i = 0; i < numRows; i++)
+		for (int j = 0; j < numCols; j++)
+			tableItems[j+i*numCols]->tableWidgetItem->setFont(wordFont);
+
 }
 
 void wordsTableWidget::prepareForStart()
 {
-	QFont wordFont = wordCells[0][0]->font();
+	QFont wordFont = tableItems[0]->tableWidgetItem->font();
 	wordFont.setPointSize(6);
 	changeFontSize(wordFont);
 
-  for (int i = 0; i < FIXED_ROWS; i++)
-    for (int j = 0; j < FIXED_COLS; j++)
-      wordCells[i][j]->setFont(wordFont);
+	for (int i = 0; i < numRows; i++)
+		for (int j = 0; j < numCols; j++)
+			tableItems[j+i*numCols]->tableWidgetItem->setFont(wordFont);
 }
 
 
 void wordsTableWidget::changeFontSize(QFont& wordFont)
 {
 
-  QString testStr(wordLength, 'M');
-  
-  bool stillFits = true;
-  while (stillFits)
-    {
-      wordFont.setPointSize(wordFont.pointSize() + 1);
-      QFontMetrics fm(wordFont);
-      if (fm.boundingRect(testStr).width() < 135 && fm.boundingRect(testStr).height() < 20)
-	stillFits = true;
-      else
-	stillFits = false;
-	  qDebug() << "Test rectangle for" << testStr << "point size" << wordFont.pointSize() 
+	QString testStr(wordLength, 'M');
+
+	bool stillFits = true;
+	while (stillFits)
+	{
+		wordFont.setPointSize(wordFont.pointSize() + 1);
+		QFontMetrics fm(wordFont);
+		if (fm.boundingRect(testStr).width() < 135 && fm.boundingRect(testStr).height() < 20)
+			stillFits = true;
+		else
+			stillFits = false;
+		qDebug() << "Test rectangle for" << testStr << "point size" << wordFont.pointSize() 
 			<< "width" << fm.boundingRect(testStr).width() << "height" << fm.boundingRect(testStr).height();
-    }
-  
+	}
+
 	qDebug() << "Set point size to " << wordFont.pointSize();
-  
+
 }
 
 
 void wordsTableWidget::clearCells()
 {
-  for (int i = 0; i < FIXED_ROWS; i++)
-    for (int j = 0; j < FIXED_COLS; j++)
-      item(i, j)->setText("");
+	for (int i = 0; i < numRows; i++)
+		for (int j = 0; j < numCols; j++)
+			item(i, j)->setText("");
 
 }
 
 
 QStringList wordsTableWidget::getCellSolutions(int i, int j)
 {
-  return cellSolutions[i][j];
+	QStringList x;
+	if (i > numRows - 1 || j > numCols - 1 || i < 0 || j < 0) return x;
+	return tableItems[j+i*numCols]->cellSolutions;
 }
 
 QString wordsTableWidget::getCellAlphagram(int i, int j)
 {
-  return cellAlphagrams[i][j];
+	QString x;
+	if (i > numRows - 1 || j > numCols - 1 || i < 0 || j < 0) return x;
+	return tableItems[j+i*numCols]->cellAlphagram;
 }
 quint8 wordsTableWidget::getCellNumSolutions(int i, int j)
 {
-
-  return cellNumSolutions[i][j];
+	if (i > numRows - 1 || j > numCols - 1 || i < 0 || j < 0) return 0;
+	return tableItems[j+i*numCols]->cellNumSolutions;
 }
 
 quint8 wordsTableWidget::getColorBrushIndex(quint8 numSolutions)
 {
-  Q_ASSERT(numSolutions > 0);
-  if (numSolutions > 9) return 8;
-  else return (numSolutions - 1);
+	Q_ASSERT(numSolutions > 0);
+	if (numSolutions > 9) return 8;
+	else return (numSolutions - 1);
 }
 
 void wordsTableWidget::answeredCorrectly(int row, int column)
 {
-  Q_ASSERT(cellNumSolutions[row][column] > 0);
-  cellNumSolutions[row][column]--;
-  int numSolutions = cellNumSolutions[row][column];
-  if (numSolutions > 0)
-    {      
-      item(row, column)->setForeground(colorBrushes[getColorBrushIndex(numSolutions)]);
-    }
-  else
-    item(row, column)->setText("");
+	tableItems[column+row*numCols]->cellNumSolutions--;
+	int numSolutions = tableItems[column+row*numCols]->cellNumSolutions;
+	if (numSolutions > 0)
+	{      
+		item(row, column)->setBackground(colorBrushes[getColorBrushIndex(numSolutions)]);
+	}
+	else
+		item(row, column)->setText("");
 }
 
 void wordsTableWidget::setCellProperties(int i, int j, QString alphagram, QStringList solutions, quint8 numSolutionsNotYetSolved)
 {
-  cellAlphagrams[i][j] = alphagram;
-  if (numSolutionsNotYetSolved > 0) 
-    {
-      wordLength = alphagram.length();
-      item(i, j)->setText(alphagram);
-      item(i, j)->setForeground(colorBrushes[getColorBrushIndex(numSolutionsNotYetSolved)]);
-    }
-  else item(i, j)->setText("");
-  cellNumSolutions[i][j] = numSolutionsNotYetSolved;
-  cellSolutions[i][j] = solutions;
+	if (i > numRows - 1 || j > numCols - 1 || i < 0 || j < 0) return;
+	tableItems[j+i*numCols]->cellAlphagram = alphagram;
+	if (numSolutionsNotYetSolved > 0) 
+	{
+		wordLength = alphagram.length();
+		item(i, j)->setText(alphagram);
+		item(i, j)->setBackground(colorBrushes[getColorBrushIndex(numSolutionsNotYetSolved)]);
+	}
+	else item(i, j)->setText("");
+	tableItems[j+i*numCols]->cellNumSolutions = numSolutionsNotYetSolved;
+	tableItems[j+i*numCols]->cellSolutions = solutions;
 
 }
 
@@ -192,28 +203,28 @@ void wordsTableWidget::setCellProperties(int i, int j, QString alphagram, QStrin
 void wordsTableWidget::alphagrammizeWords()
 {
 	// wordsWidget
-  for (int i = 0; i < FIXED_ROWS; i++)
-    for (int j = 0; j < FIXED_COLS; j++)
-      if (cellNumSolutions[i][j] != 0) item(i,j)->setText(cellAlphagrams[i][j]);
+	for (int i = 0; i < numRows; i++)
+		for (int j = 0; j < numCols; j++)
+			if (tableItems[j+i*numCols]->cellNumSolutions != 0) item(i,j)->setText(tableItems[j+i*numCols]->cellAlphagram);
 }
 
 void wordsTableWidget::shuffleWords()
 {
-  for (int i = 0; i < FIXED_ROWS; i++)
-    for (int j = 0; j < FIXED_COLS; j++)
-      item(i,j)->setText(shuffleString(item(i,j)->text()));
+	for (int i = 0; i < numRows; i++)
+		for (int j = 0; j < numCols; j++)
+			item(i,j)->setText(shuffleString(item(i,j)->text()));
 }
 
 QString wordsTableWidget::shuffleString(QString inputString)
 {
-  for (int i = 0; i < inputString.length(); i++)
-    {
-      int j = qrand() % inputString.length();
-      QChar tmp;
-      tmp = inputString[i];
-      inputString[i] = inputString[j];
-      inputString[j] = tmp;
-      
-    }
-  return inputString;
+	for (int i = 0; i < inputString.length(); i++)
+	{
+		int j = qrand() % inputString.length();
+		QChar tmp;
+		tmp = inputString[i];
+		inputString[i] = inputString[j];
+		inputString[j] = tmp;
+
+	}
+	return inputString;
 }
