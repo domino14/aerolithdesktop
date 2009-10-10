3a10c2f64914a231ad3d6efb7635bcf5776cab28
cesar <cesar@93b1bf58-7229-3f38-526f-075f678e574b>
2009-10-10 20:53:52 +0000

    test

--- flashcards/resizehandle.cpp
+++ flashcards/resizehandle.cpp
@@ -21,16 +21,11 @@ void SingleResizeRectangle::mouseReleaseEvent(QGraphicsSceneMouseEvent* event)
     emit releasedMouse();
 }
 
-
-
-
-
-
 /*************************/
 ResizeHandle::ResizeHandle(QGraphicsItem * parent)
 {
-    brHandle = new SingleResizeRectangle(QRect(-4, -4, 8, 8), parent);
-    tlHandle = new SingleResizeRectangle(QRect(-4, -4, 8, 8), parent);
+    brHandle = new SingleResizeRectangle(QRect(-4, -4, 8, 8));
+    tlHandle = new SingleResizeRectangle(QRect(-4, -4, 8, 8));
 
     brHandle->setPen(QPen(Qt::black));
     tlHandle->setPen(QPen(Qt::black));
@@ -44,14 +39,33 @@ ResizeHandle::ResizeHandle(QGraphicsItem * parent)
     connect(brHandle, SIGNAL(releasedMouse()), this, SIGNAL(stoppedMovingHandles()));
     connect(tlHandle, SIGNAL(releasedMouse()), this, SIGNAL(stoppedMovingHandles()));
 
+    showHandles(false);
+
 }
 
-void ResizeHandle::moveHandles(QRectF boundingRect)
+ResizeHandle::~ResizeHandle()
 {
-
-    double brPtx, brPty;
-    brPtx = boundingRect.x() + boundingRect.width();
-    brPty = boundingRect.y() + boundingRect.height();
-
-    brHandle->setPos(brPtx, brPty);
+    brHandle->deleteLater();
+    tlHandle->deleteLater();
 }
+
+void ResizeHandle::showHandles(bool s)
+{
+    if (s)
+    {
+        brHandle->show();
+        tlHandle->show();
+    }
+    else
+    {
+        brHandle->hide();
+        tlHandle->hide();
+    }
+}
+
+void ResizeHandle::moveHandles(double tlX, double tlY, double brX, double brY)
+{
+    tlHandle->setPos(tlX, tlY);
+    brHandle->setPos(brX, brY);
+}
+
