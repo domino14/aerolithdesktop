e0298fb668ac5ac9787f91457dc01fca6e7bff8e
cesar <cesar@93b1bf58-7229-3f38-526f-075f678e574b>
2010-09-25 06:11:49 +0000

    workin on server...

--- wordgridsServer/wordgridstable.cpp
+++ wordgridsServer/wordgridstable.cpp
@@ -2,12 +2,17 @@
 
 WordgridsTable::WordgridsTable(QObject* parent) : QObject(parent)
 {
+    gameTimer = new QTimer(this);
+    connect(gameTimer, SIGNAL(timeout()), this, SLOT(timeout()));
 }
 
 void WordgridsTable::initialize(ClientSocket *socket, quint16 tablenum, int boardSize)
 {
     this->tableNum = tablenum;
     this->boardSize = boardSize;
+    this->timerModeGame = MODE_BEGINNING;
+    gameTimer->start(2000);
+
 }
 
 void WordgridsTable::removePersonFromTable(ClientSocket* socket)
