78ba75cab90793fd8986616a651e05c8c537269a
cesar <cesar@93b1bf58-7229-3f38-526f-075f678e574b>
2010-09-25 06:11:49 +0000

    workin on server...

--- wordgridsServer/wordgridstable.h
+++ wordgridsServer/wordgridstable.h
@@ -3,8 +3,10 @@
 
 #include <QObject>
 #include "ClientSocket.h"
+#include <QTimer>
 class WordgridsTable : public QObject
 {
+    Q_OBJECT
 public:
     WordgridsTable(QObject* parent);
     void initialize(ClientSocket*, quint16, int);
@@ -12,9 +14,20 @@ public:
     QList <ClientSocket*> peopleInTable;
     void cleanupBeforeDelete();
     void personJoined(ClientSocket*);
-    int boardSize;
+
+
 private:
+    enum GameTimerModes
+    {
+        MODE_BEGINNING, MODE_INGAME, MODE_BREAK
+    };
+
     quint16 tableNum;
+    int boardSize;
+    QTimer* gameTimer;
+    bool timerModeGame;
+private slots:
+    void timeout();
 };
 
 #endif // WORDGRIDSTABLE_H
